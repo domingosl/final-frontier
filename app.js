84cd6d5000235b07b079e45c574497ddf9c1f633
domingosl <ing.domingo.sl@gmail.com>
2022-06-13 14:46:39 +0200

    update

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
 
+import * as dat from 'dat.gui';
+
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
@@ -9,6 +11,8 @@ import { PLYLoader } from 'three/examples/jsm/loaders/PLYLoader'
 
 //https://www.youtube.com/watch?v=Q7AOvWpIVHU
 
+const gui = new dat.GUI();
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -17,40 +21,21 @@ const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector("#bg")
 });
 
-const controls = new OrbitControls(camera, renderer.domElement)
+//const controls = new OrbitControls(camera, renderer.domElement)
 
-
-const spotLight = new THREE.SpotLight( 0xffffff, 2.4 );
-spotLight.position.set( 0, 300, 100 );
-
-spotLight.castShadow = true;
-
-spotLight.shadow.mapSize.width = 1024;
-spotLight.shadow.mapSize.height = 1024;
-
-spotLight.shadow.camera.near = 500;
-spotLight.shadow.camera.far = 4000;
-spotLight.shadow.camera.fov = 30;
-
-scene.add( spotLight );
-
-
-const helper = new THREE.SpotLightHelper( spotLight, 255 );
-scene.add( helper );
 
 
 const light = new THREE.AmbientLight( 0x404040 ); // soft white light
 scene.add( light );
 
 const gltfLoader = new GLTFLoader();
-const plyLoader = new PLYLoader();
 
 
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-camera.position.setZ(300);
-camera.position.setY(100);
+camera.position.setZ(50);
+//camera.position.setY(100);
 
 window.addEventListener( 'resize', onWindowResize, false );
 
@@ -63,19 +48,23 @@ function onWindowResize(){
 
 }
 
-let mainObj;
+const geometry = new THREE.SphereGeometry(10, 32, 32);
+const material = new THREE.MeshPhongMaterial();
+material.map    = THREE.ImageUtils.loadTexture('img/earth_night.jpg');
+
+const earthmesh = new THREE.Mesh(geometry, material);
+
+scene.add( earthmesh );
+
+gui.add(earthmesh.rotation, 'x').min(0).max(10);
+
+/*let mainObj;
 gltfLoader.load(
-    'models/spacesuit/scene.gltf',
+    'models/wonderful_world/scene.gltf',
     function ( gltf ) {
 
         scene.add( gltf.scene );
         mainObj = gltf.scene;
-
-        gltf.animations; // Array<THREE.AnimationClip>
-        gltf.scene; // THREE.Group
-        gltf.scenes; // Array<THREE.Group>
-        gltf.cameras; // Array<THREE.Camera>
-        gltf.asset; // Object
 
     },
     function ( xhr ) {
@@ -84,7 +73,7 @@ gltfLoader.load(
     function ( error ) {
         console.log( 'An error happened', error );
     }
-);
+);*/
 
 
 function animate() {
